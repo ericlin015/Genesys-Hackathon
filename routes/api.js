989a42eb43cbfcf8a635995b5a052fe1cc4af1ec
Juhwan Jeong <j7jeong@uwaterloo.ca>
2014-07-18 21:56:54 -0400

    New event

--- routes/api.js
+++ routes/api.js
@@ -1,28 +1,41 @@
 /*
  * Serve JSON to our AngularJS client
  */
+var http = require('http');
 
-
-exports.name = function (req, res) {
-  res.json({
-    name: 'Bob'
-  });
+exports.name = function(req, res) {
+    res.json({
+        name: 'Bob'
+    });
 };
 
 var id = 0,
-	users = [];
+    users = [],
+    events = [],
+    listOfSports = ["Soccer", "Basketball", "Tennis"];
 
-exports.createUser = function (req, res) {
-	
-	users.push({
-		userId: id,
-		name: req.body.userName,
-		postalCode: req.body.postalCode
-	});
+exports.createUser = function(req, res) {
+    users.push({
+        userId: id,
+        name: req.body.userName,
+        postalCode: req.body.postalCode
+    });
 
-	res.json({
-		userId: id++
-	});
+    res.json({
+        userId: id++
+    });
+
+exports.createEvent = function(req, res) {
+    events.push({
+        eventName: req.body.eventName,
+        location: { req.body.lat, req.body.lon },
+        sport: req.body.sport,
+        maxCapacity: req.body.capacity,
+        price: req.body.price
+    });
+
+    res.json({
+
+    });
 }
 
-exports.
