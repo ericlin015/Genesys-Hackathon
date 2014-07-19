c82c6c0ded892fe9e6bc3a29c2ec851060f51732
Eric Lin <tinyjingle@gmail.com>
2014-07-19 03:00:00 -0400

    Update angular libraries

--- public/js/controllers/home-ctrl.js
+++ public/js/controllers/home-ctrl.js
@@ -2,7 +2,21 @@ angular.module('myApp.controllers').controller(
 
     'HomeCtrl',
 
-    function($scope) {
+    function($scope, $location, cookieService) {
+
+        $scope.init = function() {
+            cookieService.checkUser(function(data) {
+                if (data) {
+                    $location.path('/mainPage');
+                }
+            });
+        };
+
+        $scope.signIn = function() {
+
+        };
+
+        $scope.init();
 
     }
 );
